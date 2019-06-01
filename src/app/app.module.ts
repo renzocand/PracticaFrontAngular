7a8a0b839e52e5924144d6c1050252ab4e9e387e
Renzo <renzocand@gmail.com>
2019-06-01 12:27:31 -0500

    Terminando primera parte del curso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-//Rutas
+//Modules
+import { PagesModule } from './pages/pages.module';
 
+//Rutas
+import { FeatureRoutingModule } from './app.routes';
+
+//Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { FeatureRoutingModule } from './app.routes';
+import { RegisterComponent } from './login/register.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    FeatureRoutingModule
+    FeatureRoutingModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,22 @@
-import { Routes, RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from "@angular/router";
+import { NgModule } from "@angular/core";
 
+import { LoginComponent } from "./login/login.component";
+import { NopagefoundComponent } from "./shared/nopagefound/nopagefound.component";
+import { RegisterComponent } from './login/register.component';
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
 
 const routes: Routes = [
-    { path: '', component: DashboardComponent },
-    { path: 'dashboard', component: DashboardComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: LoginComponent },
-    { path: 'progress', component: ProgressComponent },
-    { path: 'graficas1', component: Graficas1Component },
-    { path: '**', component: NopagefoundComponent },
 
+  { path: "", component: PagesComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "**", component: NopagefoundComponent }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, {useHash:true})],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule]
 })
 export class FeatureRoutingModule {}
-
-
